50e3fa35b1f2cb82237b1844a3691afdc9d9539a
meerlin <meerlin.meers@gmail.com>
2017-10-21 05:52:11 +0200

    Added setup.py and requirements.txt.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ws_parsepage',
+    version='1.0.0',
+    packages=['ws_parsepage'],
+    url='',
+    license='',
+    author='Meerlin Meers',
+    author_email='meerlin.meers@gmail.com',
+    description=''
+)
